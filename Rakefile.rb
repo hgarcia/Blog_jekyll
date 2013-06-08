3d320825cfbea33abaaca80e3ef737c67f83b52e
hgarcia <hernan@dynamicprogrammer.com>
2013-06-08 05:24:15 +0100

    Build

--- Rakefile.rb
+++ Rakefile.rb
@@ -20,12 +20,12 @@ namespace :build do
 
   desc 'Pre building the site for local testing'
 	task :pre => [:merge_and_minimize_css] do
-		sh 'jekyll'
+		sh 'jekyll build'
 	end
 
 	desc 'Minimizing and combining css files'
 	task :merge_and_minimize_css do
-		sh 'juicer merge assets/css/master.css --force'
+		#sh 'juicer merge assets/css/master.css --force'
 	end
 
 	desc 'Add changes commit and push to github'
