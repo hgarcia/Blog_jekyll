39f3f67afd4cf5e9e81f6195ad44b839758b8f86
hgarcia <hernan@dynamicprogrammer.com>
2013-06-22 02:22:58 -0400

    Working on image gallery

--- _plugins/flickr.rb
+++ _plugins/flickr.rb
@@ -8,44 +8,24 @@ CACHED_IMAGES = {}
 
 module Flickr
 
-  def flick_url(image_id)
+  def flickr_url(image_id)
     "http://www.flickr.com/photos/theprogrammer/#{image_id}"
   end
 
-  def flickr_original(image_id)
+  def flickr_img(image_id, size = nil)
     img = image_object(image_id)
-    image_tag(img.title, img.original.url)
-  end
-
-  def flickr_large(image_id)
-    img = image_object(image_id)
-    image_tag(img.title, img.large.url)
-  end
-
-  def flickr_medium(image_id)
-    img = image_object(image_id)
-    image_tag(img.title, img.medium.url)
-  end
-
-  def flickr_small(image_id)
-    img = image_object(image_id)
-    image_tag(img.title, img.small.url)
-  end
-
-  def flickr_thumb(image_id)
-    img = image_object(image_id)
-    image_tag(img.title, img.thumbnail.url)
-  end
-
-  def flickr_square(image_id)
-    img = image_object(image_id)
-    image_tag(img.title, img.square.url)
+    url = img.send(size).url if img.respond_to? size
+    image_tag(img.title, url)
   end
 
   private
 
+  def get_size_segment(symbol)
+    SIZE_SEGMENTS[symbol]
+  end
+
   def image_object(image_id)
-    url = flick_url(image_id)
+    url = flickr_url(image_id)
     CACHED_IMAGES[url] ||= Fleakr.resource_from_url(url)
   end
 
