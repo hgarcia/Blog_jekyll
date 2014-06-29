869cbeb98118fc72e587ce522b5c1bf6608a33dc
hgarcia <hernan@dynamicprogrammer.com>
2014-06-28 23:50:53 -0400

    Build

--- _plugins/flickr.rb
+++ _plugins/flickr.rb
@@ -1,46 +1,46 @@
 require 'liquid'
 require 'flickr'
 
-API_KEY = 'fef6644edfac9a21a909f8152719aaa3'
-CACHED_IMAGES = {}
-SIZES = {:square => "Square", :large_square => "Large Square", :thumbnail => "Thumbnail", :small =>
-"Small", :small320 => "Small 320", :medium => "Medium", :medium640 => "Medium 640", :medium800 => "Medium 640", :large => "Large", :large1600 => "Large", :large2048 => "Large", :original => "Large", :panoramic => "Large"}
+# API_KEY = 'fef6644edfac9a21a909f8152719aaa3'
+# CACHED_IMAGES = {}
+# SIZES = {:square => "Square", :large_square => "Large Square", :thumbnail => "Thumbnail", :small =>
+# "Small", :small320 => "Small 320", :medium => "Medium", :medium640 => "Medium 640", :medium800 => "Medium 640", :large => "Large", :large1600 => "Large", :large2048 => "Large", :original => "Large", :panoramic => "Large"}
 
 module FlickrM
   @printed = false
 
   def flickr_img(image_id, size = :medium, attrs = {})
-    img = image_object(image_id, get_size_segment(size.downcase.to_sym))
-    puts img
-    attrs[:style] = "height: 175px;" if (size == "panoramic")
-    image_tag(img[:title], img[:url], attrs)
+    # img = image_object(image_id, get_size_segment(size.downcase.to_sym))
+    # puts img
+    # attrs[:style] = "height: 175px;" if (size == "panoramic")
+    # image_tag(img[:title], img[:url], attrs)
   end
 
-  private
+  # private
 
-  def get_size_segment(symbol)
-    SIZES[symbol]
-  end
+  # def get_size_segment(symbol)
+  #   SIZES[symbol]
+  # end
 
-  def image_object(image_id, size)
-      begin
-        img = CACHED_IMAGES[image_id] ||= Photo.new(image_id, API_KEY)
-        return {:title => img.title, :url => img.size_url(size)}
-      rescue => e
-        p "IMAGE NOT FOUND: id: #{image_id} - size: #{size} - url: #{url}"
-        {:title => "not found", :url => "#"}
-      end
-  end
+  # def image_object(image_id, size)
+  #     begin
+  #       img = CACHED_IMAGES[image_id] ||= Photo.new(image_id, API_KEY)
+  #       return {:title => img.title, :url => img.size_url(size)}
+  #     rescue => e
+  #       p "IMAGE NOT FOUND: id: #{image_id} - size: #{size} - url: #{url}"
+  #       {:title => "not found", :url => "#"}
+  #     end
+  # end
 
-  def image_tag(title, url, attrs)
-    "<img alt='#{title}' src='#{url}' #{image_attrs(attrs)}>"
-  end
+  # def image_tag(title, url, attrs)
+  #   "<img alt='#{title}' src='#{url}' #{image_attrs(attrs)}>"
+  # end
 
-  def image_attrs(attrs)
-    string_of_attributes = ""
-    attrs.each {|k, v| string_of_attributes += "#{k}=\"#{v}\""}
-    string_of_attributes
-  end
+  # def image_attrs(attrs)
+  #   string_of_attributes = ""
+  #   attrs.each {|k, v| string_of_attributes += "#{k}=\"#{v}\""}
+  #   string_of_attributes
+  # end
 end
 
 Liquid::Template.register_filter(FlickrM)
